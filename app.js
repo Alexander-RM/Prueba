1a2cd90d8ed5ca8586464dbd72362dfc7669eb85
Alexander-RM <alexander.reyes@agilethought.com>
2021-09-07 17:59:31 -0500

    commit de prueba

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
 //const hostname = '127.0.0.1';
-const hostname = 'localhost';
+//const hostname = 'localhost';
+const hostname = 'ec2-34-203-246-102.compute-1.amazonaws.com';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola R&D, hola a todos todos todos todos');
+  res.end('Hola R&D, hola a todos todos todos todos todostodostodostodostodostodostodos');
 });
 
 server.listen(port, hostname, () => {
