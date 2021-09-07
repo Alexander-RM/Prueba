be1dd6cf6c0e8b3fe57fe879e5f9b1cef1790295
Alexander-RM <alexander.reyes@agilethought.com>
2021-09-07 16:16:33 -0500

    commit de prueba

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo 7');
+  res.end('Hola R&D');
 });
 
 server.listen(port, hostname, () => {
