fc9eaa35ce0d538a5a94fe78935ab5912d254697
Alexander-RM <alexander.reyes@agilethought.com>
2021-09-14 18:38:26 -0500

    agregando nuevo cambio 14/09/21

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-  res.send('Hello a todos todostodostodostodostodostodos y todas !')
+  res.send('hola a todos, mañana empieza el puente!')
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
